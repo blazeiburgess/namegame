34ff4c7562e2217c00a3443f50aa55ea58c1652f
blaze.i.burgess <blaze.i.burgess@gmail.com>
2016-11-20 10:02:59 -0500

    Fix typo in index, add function to pull 5 persons from api return

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,21 @@ function main() {
 }
 
 function makePersonsFromJSON(personsArray) {
-  console.log(personsAray);
+  var selectionOfFive = [];
+  var usedIndexes = [];
+  for (var i = 0; i < 5; i++) {
+    var randPerson = getRandomFromArray(personsArray);
+    if (selectionOfFive.indexOf(randPerson) >= 0) {
+      i--;
+    } else {
+      selectionOfFive.push(randPerson);
+    }
+  }
+  console.log(selectionOfFive);
 }
 
+function getRandomFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+main();
