e3d0e6440ab79c63f8ec5418ae7ca2e6df175dde
blaze.i.burgess <blaze.i.burgess@gmail.com>
2016-11-20 09:54:02 -0500

    Add ajax request and function to handle return data

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function main() {
+  $.getJSON('http://api.namegame.willowtreemobile.com/', function(result) { 
+    makePersonsFromJSON(result);
+  });
+}
+
+function makePersonsFromJSON(personsArray) {
+  console.log(personsAray);
+}
+
